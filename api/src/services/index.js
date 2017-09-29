5237ee202d9b633392980d12de727df9d904a75e
Luc Claustres <luc.claustres@orange.fr>
2017-09-29 09:22:47 +0200

    Added plugins for device/push management on cordova

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,10 +1,33 @@
+import _ from 'lodash'
 import logger from 'winston'
 import kCore from 'kCore'
 import kTeam from 'kTeam'
-import kNotify from 'kNotify'
+import kNotify, { hooks } from 'kNotify'
 
 module.exports = async function () {
   const app = this
+
+  app.on('service', service => {
+    // Add hooks for topic creation/removal on org object creation/removal
+    if (service.name === 'groups' || service.name === 'organisations') {
+      service.hooks({
+        after: {
+          create: hooks.createTopic,
+          remove: hooks.removeTopic
+        }
+      })
+    }
+    // Add hooks for topic (un)subscription on (un)authorisation
+    if (service.name === 'authorisations') {
+      service.hooks({
+        after: {
+          create: hooks.subscribeSubjectsToResourceTopic,
+          remove: hooks.unsubscribeSubjectsFromResourceTopic
+        }
+      })
+    }
+  })
+
   // Set up our plugin services
   try {
     await app.configure(kCore)
@@ -16,6 +39,7 @@ module.exports = async function () {
   }
 
   let usersService = app.getService('users')
+  let pusherService = app.getService('pusher')
 
   // Create default users if not already done
   await usersService.find({
@@ -26,7 +50,19 @@ module.exports = async function () {
       let createdUser = users.find(user => user.email === defaultUser.email)
       if (!createdUser) {
         logger.info('Initializing default user (email = ' + defaultUser.email + ', password = ' + defaultUser.password + ')')
-        usersService.create(defaultUser)
+        usersService.create(_.omit(defaultUser, 'device'))
+        .then(user => {
+          // Register user device if any
+          if (defaultUser.device) {
+            pusherService.create({
+              action: 'device',
+              deviceId: defaultUser.device.registrationId,
+              devicePlatform: defaultUser.device.platform
+            }, {
+              user
+            })
+          }
+        })
       }
     })
   })
