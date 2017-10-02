bf17caeea08d12edb4ecd2d757331e85c750bfe6
Luc Claustres <luc.claustres@orange.fr>
2017-10-02 17:36:44 +0200

    Fixed initialization order for modules

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -13,7 +13,22 @@ module.exports = async function () {
   // Set up our plugin services
   try {
     await app.configure(kCore)
+    // Add hook to automatically create a new organisation, add verification, send verification email,
+    // register devices, etc. when creating a new user or authenticating
+    app.configureService('users', app.getService('users'), servicesPath)
+    app.configureService('authentication', app.getService('authentication'), servicesPath)
+  
+    // Add hooks for topic creation/removal on org/group object creation/removal
+    app.on('service', service => {
+      console.log(service.name)
+      if (service.name === 'groups' || service.name === 'organisations') {
+        app.configureService(service.name, service, servicesPath)
+      }
+    })
     await app.configure(kTeam)
+    // Add hooks for topic (un)subscription on (un)authorisation
+    app.configureService('authorisations', app.getService('authorisations'), servicesPath)
+
     await app.configure(kNotify)
   }
   catch (error) {
@@ -21,22 +36,7 @@ module.exports = async function () {
   }
 
   let usersService = app.getService('users')
-  let authenticationService = app.getService('authentication')
-  let autorisationsService = app.getService('authorisations')
   let pusherService = app.getService('pusher')
-
-  // Add hook to automatically create a new organisation, add verification, send verification email,
-  // register devices, etc. when creating a new user or authenticating
-  app.configureService('users', usersService, servicesPath)
-  app.configureService('authentication', authenticationService, servicesPath)
-  // Add hooks for topic (un)subscription on (un)authorisation
-  app.configureService('authorisations', autorisationsService, servicesPath)
-  // Add hooks for topic creation/removal on org/group object creation/removal
-  app.on('service', service => {
-    if (service.name === 'groups' || service.name === 'organisations') {
-      app.configureService(service.name, service, servicesPath)
-    }
-  })
 
   // Create default users if not already done
   let users = await usersService.find({ paginate: false })
