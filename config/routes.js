f33ef8468ae65a175b06bb90cd322ef51529745f
Christophe Nouguier <christophe.nouguier@free.fr>
2018-01-08 14:18:50 +0100

    Fixed route issue

--- config/routes.js
+++ config/routes.js
@@ -87,7 +87,8 @@ module.exports = {
                 props: true,
                 children: {
                   'create': { name: 'create-group', component: 'editor/KModalEditor', props: true },
-                  'edit/:id/:perspective?': { name: 'edit-group', component: 'editor/KModalEditor', props: true }
+                  'edit/:id/:perspective?': { name: 'edit-group', component: 'editor/KModalEditor', props: true },
+                  'explore/:id/:perspective': { name: 'explore-group', component: 'KGroupExplorer', props: true }
                 }
               },
               'events': {
@@ -95,7 +96,7 @@ module.exports = {
                 component: 'KEventsActivity',
                 props: true,
                 children: {
-                  'create:/templateId': { name: 'create-event', component: 'KModalEventEditor', props: true },
+                  'create/:templateId': { name: 'create-event', component: 'KModalEventEditor', props: true },
                   'edit/:id/:perspective?': { name: 'edit-event', component: 'KModalEventEditor', props: true }
                 }
               },
