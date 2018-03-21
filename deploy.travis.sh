07203a5d54e9df85ba17966ea9f72cd219e6af4d
Luc Claustres <luc.claustres@orange.fr>
2018-03-21 09:49:44 +0100

    Try to fix SSH prompting and added conditional stages on env var

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -2,7 +2,7 @@
 eval "$(ssh-agent -s)"
 chmod 600 ssh.pem
 ssh-add ssh.pem
-ssh ${SSH_USER}@${SSH_REMOTE} mkdir -p kApp
+ssh -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_REMOTE} mkdir -p kApp
 scp docker-compose.yml ${SSH_USER}@${SSH_REMOTE}:~/kApp/docker-compose.yml
 scp docker-compose.deploy.yml ${SSH_USER}@${SSH_REMOTE}:~/kApp/docker-compose.deploy.yml
 scp kApp.env.sh ${SSH_USER}@${SSH_REMOTE}:~/kApp/kApp.env.sh
