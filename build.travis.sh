1c445e99175ec80b429fd0ec197a87a5f50507dd
Luc Claustres <luc.claustres@orange.fr>
2018-04-04 22:27:17 +0200

    WIP https://github.com/kalisio/kApp/issues/34 (manage multiple versions) [skip build][skip test]

--- build.travis.sh
+++ build.travis.sh
@@ -2,13 +2,15 @@
 if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip build]"* ]]
 then
 	echo "Skipping build stage"
-else
-	source env.travis.sh
-	# If you'd like to simply pull existing version instead of really build it
-	#- docker pull kalisio/kapp
-	docker-compose -f docker-compose.yml up -d
-	docker cp kapp:/opt/kapp/dist dist
-	docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-	docker tag kalisio/kapp kalisio/kapp:${FLAVOR}
-	docker push kalisio/kapp:${FLAVOR}
+	# We simply pull existing version instead of really build it
+	# Indeed we cannot really skip the build otherwise the deploy step will fail due to missing artefacts
+	docker pull kalisio/kapp
 fi
+
+source env.travis.sh
+docker-compose -f docker-compose.yml up -d
+docker cp kapp:/opt/kapp/dist dist
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
+docker tag kalisio/kapp kalisio/kapp:${FLAVOR}
+docker push kalisio/kapp:${FLAVOR}
+
