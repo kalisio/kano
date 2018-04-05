558e5b6436512391cee40a992ad69fb52c92c600
Luc Claustres <luc.claustres@orange.fr>
2018-04-05 17:52:02 +0200

    Fixed potential bugs on tags

--- api/src/services/tags/tags.hooks.js
+++ api/src/services/tags/tags.hooks.js
@@ -17,11 +17,11 @@ module.exports = {
     find: [],
     get: [],
     // We create topics for members tag only, check if tag is really created or just its count increased
-    create: [ when(hook => hook.result.scope === 'members' && hook.result.count === 1, hooks.createTopic) ],
+    create: [ when(hook => hook.result && hook.result.scope === 'members' && hook.result.count === 1, hooks.createTopic) ],
     update: [],
     patch: [],
     // We remove topics for members tag only, check if tag is really removed or just its count decreased
-    remove: [ when(hook => hook.result.scope === 'members' && hook.result.count <= 0, hooks.removeTopic) ]
+    remove: [ when(hook => hook.result && hook.result.scope === 'members' && hook.result.count <= 0, hooks.removeTopic) ]
   },
 
   error: {
