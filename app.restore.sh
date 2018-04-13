92a82148e4b86c7a7a6523c2e12df66ed02612ce
Luc Claustres <luc.claustres@orange.fr>
2018-04-13 16:17:31 +0200

    Added backup/restore scripts [skip ci]

---
+++ app.restore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+USAGE_MESSAGE="Usage: $0 [-h] [-d dbArchive]
+ -h help
+ -d {dbArchive} to use"
+
+while getopts hd: args
+do case "$args" in
+  d) BACKUP_ARCHIVE="$OPTARG";;
+  h) echo "${USAGE_MESSAGE}"
+     exit 1;;
+  :) echo "${USAGE_MESSAGE}"
+     exit 1;;
+  *) echo "${USAGE_MESSAGE}"
+     exit 1;;
+esac
+done
+
+shift $(($OPTIND - 1))
+
+docker cp ./dumps/${BACKUP_ARCHIVE} mongodb-aktnmap:/dumps/${BACKUP_ARCHIVE}
+docker exec -t mongodb-aktnmap mongorestore --gzip --archive=/dumps/${BACKUP_ARCHIVE}
