db73920ada6b7f48aab9af37c8cdcdcca25e85f4
Luc Claustres <luc.claustres@orange.fr>
2018-04-16 10:10:13 +0200

    Reported fix for travis build on test flavor [skip ci]

--- build/script.clean.js
+++ build/script.clean.js
@@ -2,10 +2,15 @@ var
   shell = require('shelljs'),
   path = require('path')
 
+shell.rm('-rf', path.resolve(__dirname, '../kCore/*'))
 shell.rm('-rf', path.resolve(__dirname, '../kCore/.*'))
+shell.rm('-rf', path.resolve(__dirname, '../kTeam/*'))
 shell.rm('-rf', path.resolve(__dirname, '../kTeam/.*'))
+shell.rm('-rf', path.resolve(__dirname, '../kNotify/*'))
 shell.rm('-rf', path.resolve(__dirname, '../kNotify/.*'))
+shell.rm('-rf', path.resolve(__dirname, '../kEvent/*'))
 shell.rm('-rf', path.resolve(__dirname, '../kEvent/.*'))
+shell.rm('-rf', path.resolve(__dirname, '../kMap/*'))
 shell.rm('-rf', path.resolve(__dirname, '../kMap/.*'))
 shell.rm('-rf', path.resolve(__dirname, '../dist/*'))
 shell.rm('-rf', path.resolve(__dirname, '../dist/.*'))
