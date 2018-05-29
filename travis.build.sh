e02fd77a58d57c71e40500b1b8454fb8c458caa3
Christophe Nouguier <christophe.nouguier@free.fr>
2018-05-29 08:38:45 +0200

    Updated docker network type

--- travis.build.sh
+++ travis.build.sh
@@ -10,7 +10,7 @@ then
 fi
 
 cd deploy
-docker network create -d overlay --attachable $NETWORK}
+docker network create --attachable $NETWORK}
 docker-compose -f app.yml -f app.build.yml up -d
 docker cp $APP:/opt/$APP/dist dist
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
