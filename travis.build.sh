21f6a9a9ed4ffe15a5af4bb2966c539b25eff219
Christophe Nouguier <christophe.nouguier@free.fr>
2018-05-31 00:21:02 +0200

    Udpated build script [skip build]

--- travis.build.sh
+++ travis.build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 source travis.env.sh
 
+# NOTE: The process build the image and run the container in order to allow us to copy the 
+# built artifact from the container to the host. Indeed the artifact is then copied to S3 
+# (see the deploy hook) and can be used by the following stages (i.e. Android and iOS).
+
+# It first need to create the required network 
+docker network create --attachable $NETWORK
+
 if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip build]"* ]]
 then
 	echo "Skipping build stage"
@@ -8,23 +15,19 @@ then
 	# Indeed we cannot really skip the build otherwise the deploy step will fail due to missing artefacts
 	docker pull kalisio/$APP:$VERSION_TAG
 else 
-	# NOTE: The process build the image and run the container in order to allow us to copy the 
-	# built artifact from the container to the host. Indeed the artifact is then copied to S3 
-	# (see the deploy hook) and can be used by the following stages (i.e. Android and iOS).
-
-	# It first need to create the required network 
-	docker network create --attachable $NETWORK
-
 	# Build the image and run the container
 	docker-compose -f deploy/app.yml -f deploy/app.build.yml up -d
-
-	# Copy the artifact from the container to the host
-	# See https://docs.docker.com/compose/reference/envvars/#compose_project_name to get an 
-	# explanation on the container name
-	docker cp ${APP}_app_1:/opt/${APP}/dist dist
 
 	# Tag the built image and push it to the hub
 	docker tag kalisio/$APP kalisio/$APP:$VERSION_TAG
 	docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
 	docker push kalisio/$APP:$VERSION_TAG
 fi
+
+# Copy the artifact from the container to the host
+# See https://docs.docker.com/compose/reference/envvars/#compose_project_name
+# explanation on the container name
+docker cp ${APP}_app_1:/opt/${APP}/dist dist
+
+
+
