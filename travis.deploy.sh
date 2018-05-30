4a893e6be83aeee55766b93e0209339d226639d7
Christophe Nouguier <christophe.nouguier@free.fr>
2018-05-30 13:11:21 +0200

    Fixed remote script names [skip test]

--- travis.deploy.sh
+++ travis.deploy.sh
@@ -11,11 +11,11 @@ else
 	scp .env ${SSH_USER}@${SSH_REMOTE}:~/${APP}/.env
 	scp deploy/app.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.yml
 	scp deploy/app.swarm.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.swarm.yml
-	scp deploy/scripts/app.deploy.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.deploy.sh
-	scp deploy/scripts/app.remove.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.remove.sh
-	scp deploy/scripts/app.backup.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.backup.sh
-	scp deploy/scripts/app.restore.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.restore.sh
-	scp deploy/scripts/app.backup.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.backup.yml
+	scp deploy/scripts/app.deploy.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/deploy.sh
+	scp deploy/scripts/app.remove.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/remove.sh
+	scp deploy/scripts/app.backup.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/backup.sh
+	scp deploy/scripts/app.restore.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/restore.sh
+	scp deploy/scripts/app.backup.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/backup.yml
 	ssh ${SSH_USER}@${SSH_REMOTE} "cd ${APP}; chmod u+x ./remove.sh; chmod u+x ./deploy.sh; chmod u+x ./backup.sh; chmod u+x ./restore.sh; ./remove.sh; ./deploy.sh"
 fi
 
