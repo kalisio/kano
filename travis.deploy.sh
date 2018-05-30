4f3d1b82a7dc1f053c956b5b7e510e00737d2292
Christophe Nouguier <christophe.nouguier@free.fr>
2018-05-30 14:40:03 +0200

    Updated deploy structure [skip test] [skip ios]

--- travis.deploy.sh
+++ travis.deploy.sh
@@ -11,11 +11,8 @@ else
 	scp .env ${SSH_USER}@${SSH_REMOTE}:~/${APP}/.env
 	scp deploy/app.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.yml
 	scp deploy/app.swarm.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.swarm.yml
-	scp deploy/scripts/deploy.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/deploy.sh
-	scp deploy/scripts/remove.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/remove.sh
-	scp deploy/scripts/backup.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/backup.sh
-	scp deploy/scripts/restore.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}/restore.sh
-	scp deploy/scripts/backup.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/backup.yml
-	ssh ${SSH_USER}@${SSH_REMOTE} "cd ${APP}; chmod u+x ./remove.sh; chmod u+x ./deploy.sh; chmod u+x ./backup.sh; chmod u+x ./restore.sh; ./remove.sh; ./deploy.sh"
+	scp deploy/scripts ${SSH_USER}@${SSH_REMOTE}:~/${APP}
+	scp deploy/configs ${SSH_USER}@${SSH_REMOTE}:~/${APP}/configs
+	ssh ${SSH_USER}@${SSH_REMOTE} "cd ${APP}; chmod u+x ./remove_stack.sh; chmod u+x ./deploy_stack.sh; chmod u+x ./backup_db.sh; chmod u+x ./restore_db.sh; ./remove.sh; ./deploy.sh"
 fi
 
