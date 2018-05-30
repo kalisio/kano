13159cbf0a8a6f432af4cc52e66a32090bb93681
Christophe Nouguier <christophe.nouguier@free.fr>
2018-05-30 15:15:53 +0200

    Updated build [skip test] [skip ios]

--- travis.env.sh
+++ travis.env.sh
@@ -43,6 +43,7 @@ echo "SUBDOMAIN=$SUBDOMAIN" >> .env
 echo "NETWORK=$NETWORK" >> .env
 echo "PORT=$PORT" >> .env
 echo "BUILD_NUMBER=$TRAVIS_BUILD_NUMBER" >> .env
+echo "REPO_DIR=$TRAVIS_BUILD_DIR" >> .env
 echo "GOOGLE_MAIL_USER=$GOOGLE_MAIL_USER" >> .env
 echo "GOOGLE_MAIL_PASSWORD=$GOOGLE_MAIL_PASSWORD" >> .env
 echo "APP_SECRET=$APP_SECRET" >> .env
