c103a41f63f1d098ba1bf19b6bb223eebd453bd6
Christophe Nouguier <christophe.nouguier@free.fr>
2018-05-30 16:37:44 +0200

    Updated deploy script [skip test]

--- travis.deploy.sh
+++ travis.deploy.sh
@@ -8,11 +8,22 @@ else
 	chmod 600 ssh.pem
 	ssh-add ssh.pem
 	ssh -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_REMOTE} mkdir -p ${APP}
+
+	# Deploy environment file
 	scp .env ${SSH_USER}@${SSH_REMOTE}:~/${APP}/.env
+
+	# Deploy compose files
 	scp deploy/app.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.yml
 	scp deploy/app.swarm.yml ${SSH_USER}@${SSH_REMOTE}:~/${APP}/app.swarm.yml
-	scp deploy/scripts ${SSH_USER}@${SSH_REMOTE}:~/${APP}
-	scp deploy/configs ${SSH_USER}@${SSH_REMOTE}:~/${APP}/configs
+
+  # Deploy configs
+  scp -rp deploy/configs ${SSH_USER}@${SSH_REMOTE}:~/${APP}/configs
+
+	# Deploy utilities
+	scp deploy/scripts/backup_db.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}
+	scp deploy/scripts/restore_db.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}
+	scp deploy/scripts/deploy_stack.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}
+	scp deploy/scripts/remove_statck.sh ${SSH_USER}@${SSH_REMOTE}:~/${APP}
 	ssh ${SSH_USER}@${SSH_REMOTE} "cd ${APP}; chmod u+x ./remove_stack.sh; chmod u+x ./deploy_stack.sh; chmod u+x ./backup_db.sh; chmod u+x ./restore_db.sh; ./remove.sh; ./deploy.sh"
 fi
 
