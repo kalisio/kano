70e9434f139ecb146f37d46f472228bb67cd4315
Christophe Nouguier <christophe.nouguier@free.fr>
2018-07-09 13:09:04 +0200

    Fixed deploy files

--- deploy/deploy-app.sh
+++ deploy/deploy-app.sh
@@ -7,5 +7,5 @@ set +a
 if [ "$FLAVOR" = "dev" ]; then
   docker stack deploy -c app.yml -c app.swarm.yml -c mongodb.yml -c mongodb.swarm.yml $APP
 else
-  docker stack deploy -c app.yml -c app.swarm.yml -c $APP
+  docker stack deploy -c app.yml -c app.swarm.yml $APP
 fi
