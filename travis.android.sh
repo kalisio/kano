2ff9dfa8869680dd93ef5b57aa215e64777e445e
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-07 11:36:27 +0200

    Updated CI scripts

--- travis.android.sh
+++ travis.android.sh
@@ -4,11 +4,25 @@ then
 	echo "Skipping android stage"
 else
 	source travis.env.sh
-	export ORG_GRADLE_PROJECT_cdvVersionCode=$TRAVIS_BUILD_NUMBER
+
+	 # Retrieve the built Web app
+	aws s3 sync s3://kapp-builds/$TRAVIS_BUILD_NUMBER/dist cordova/www
+
+		# Retrieve the secret files
+	echo -e "machine github.com\n  login $GITHUB_TOKEN" > ~/.netrc
+	git clone https://github.com/kalisio/kApp-secrets
+
+		# Install the required secret files requied to sign the app
+	cp kApp-secrets/cordova/build.json cordova/.
+	cp kApp-secrets/cordova/kalisio.keystore cordova/kalisio.keystore
+
+	# Build and deploy the mobile app
+		export ORG_GRADLE_PROJECT_cdvVersionCode=$TRAVIS_BUILD_NUMBER
 	cd cordova/fastlane
 	sh android.sh
+	cd ../..
 	npm run cordova:deploy:android
 
 	# Store the android build to S3
-	aws s3 sync cordova/platforms/android/build/outputs/apk s3://kapp-builds/$TRAVIS_BUILD_NUMBER/android
+	aws s3 sync cordova/platforms/android/build/outputs s3://kapp-builds/$TRAVIS_BUILD_NUMBER/android
 fi
--- travis.ios.sh
+++ travis.ios.sh
@@ -3,21 +3,23 @@ if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip ios]"* ]]
 then
 	echo "Skipping ios stage"
 else
+	source travis.env.sh
+
   # Retrieve the built Web app
 	aws s3 sync s3://kapp-builds/$TRAVIS_BUILD_NUMBER/dist cordova/www
 
 	# Retrieve the secret files
 	echo -e "machine github.com\n  login $GITHUB_TOKEN" > ~/.netrc
 	git clone https://github.com/kalisio/kApp-secrets
-	# Install the required secret files
+
+	# Install the required secret files requied to sign the app
 	cp kApp-secrets/cordova/build.json cordova/.
-	# Generate the fastlane Appfile
 	mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 	cp kApp-secrets/cordova/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/.
 
 	# Build and deploy the app
 	npm run cordova:deploy:ios
 
-  # Store the ios build to S3
+  # Backup the ios build to S3
 	aws s3 sync cordova/platforms/ios/build s3://kapp-builds/$TRAVIS_BUILD_NUMBER/ios
 fi
