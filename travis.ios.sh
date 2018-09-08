bf90ee77e8d28b23ff0d2b27f130f57d7c116e3f
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-08 13:13:02 +0200

    Updated ios script

--- travis.ios.sh
+++ travis.ios.sh
@@ -18,18 +18,8 @@ else
 
 	# Create a custom keychain
 	security create-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN
-
-	# Unlock the keychain
 	security unlock-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN
-
-	# Set keychain timeout to 1 hour for long builds
-	# see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
-	security set-keychain-settings -t 1800 -u $KEY_CHAIN
-
-	# Set keychain search list
-	security list-keychains -s $KEY_CHAIN
-
-	# Make the custom keychain default, so xcodebuild will use it for signing
+  security -v set-keychain-settings -lut 86400 $KEY_CHAIN
 	security default-keychain -s $KEY_CHAIN
 
 	# Add certificates to keychain and allow codesign to access them
