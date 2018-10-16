e040bb67abda59697c331db0ff3a564a48f2ae16
Luc Claustres <luc.claustres@orange.fr>
2018-10-16 22:42:24 +0200

    Try to fix build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -33,12 +33,6 @@ let plugins = [
   new webpack.optimize.CommonsChunkPlugin({
     name: 'vendor',
     minChunks: Infinity
-  }),
-  // extract webpack runtime and module manifest to its own file in order to
-  // prevent vendor hash from being updated whenever app bundle is updated
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'manifest',
-    chunks: ['vendor']
   })
 ]
 
