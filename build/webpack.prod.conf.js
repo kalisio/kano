a7a7aea813ea711d60a14c0d8738592735ec10e0
Luc Claustres <luc.claustres@orange.fr>
2018-10-16 21:54:15 +0200

    Try to fix build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -32,19 +32,7 @@ let plugins = [
   // split vendor js into its own file
   new webpack.optimize.CommonsChunkPlugin({
     name: 'vendor',
-    minChunks: function (module, count) {
-      // any required modules inside node_modules are extracted to vendor
-      return (
-        module.resource &&
-        /\.js$/.test(module.resource) &&
-        (
-          module.resource.indexOf('quasar') > -1 ||
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      )
-    }
+    minChunks: Infinity
   }),
   // extract webpack runtime and module manifest to its own file in order to
   // prevent vendor hash from being updated whenever app bundle is updated
