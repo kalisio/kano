de066da24d70f86fd085f64351b77bb3d8685131
Christophe Nouguier <christophe.nouguier@free.fr>
2019-03-18 23:39:47 +0100

    build: wip #57

--- .travis.build.sh
+++ .travis.build.sh
@@ -13,21 +13,11 @@ travis_fold start "build"
 # built artifact from the container to the host. Indeed the artifact is then copied to S3 
 # (see the deploy hook) and can be used by the following stages (i.e. Android and iOS).
 
-if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip build]"* ]]
+if [[ $TRAVIS_COMMIT_MESSAGE != *"[skip build]"* ]]
 then
-	echo "Skipping build stage"
-	# We simply pull existing version instead of really build it
-	# Indeed we cannot really skip the build otherwise the deploy step will fail due to missing artefacts
-	docker-compose -f deploy/app.yml up -d
-else 
-	# Build the image and run the container
-	docker-compose -f deploy/app.yml -f deploy/app.build.yml up -d
-  # Check if the build has succeeded
-	docker exec -ti ${APP}_app_1 ls /opt/$APP/dist
-	if [ $? -eq 1 ]; then
-	  exit 1
-  fi
-
+	# Build the image
+	docker-compose -f deploy/app.yml -f deploy/app.build.yml build
+  
 	# Tag the built image and push it to the hub
 	docker tag kalisio/$APP kalisio/$APP:$VERSION_TAG
 	docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
@@ -44,7 +34,8 @@ travis_fold start "backup"
 # Copy the artifact from the container to the host
 # See https://docs.docker.com/compose/reference/envvars/#compose_project_name
 # explanation on the container name
-docker cp $APP_app_1:/opt/$APP/dist dist
+docker-compose -f deploy/app.yml up -d
+docker cp ${APP}_app_1:/opt/$APP/dist dist
 
 # Backup the artifact to S3
 aws s3 sync dist s3://$BUILDS_BUCKET/$BUILD_NUMBER/www > /dev/null
