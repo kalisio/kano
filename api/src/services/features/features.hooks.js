50c14431d30fb9514afcb5c1b9b5c4e840f09056
Luc Claustres <luc.claustres@orange.fr>
2019-04-12 18:59:37 +0200

    added hooks for feature edition

---
+++ api/src/services/features/features.hooks.js
@@ -0,0 +1,33 @@
+import { discard, setNow } from 'feathers-hooks-common'
+
+module.exports = {
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [ setNow('createdAt', 'updatedAt') ],
+    update: [ discard('createdAt', 'updatedAt'), setNow('updatedAt') ],
+    patch: [ discard('createdAt', 'updatedAt'), setNow('updatedAt') ],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [ setNow('updatedAt') ]
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+}
