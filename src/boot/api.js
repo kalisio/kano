acc29444584be0c2b1c6a6b66e9faf07ccd1f92a
Luc Claustres <luc.claustres@orange.fr>
2019-09-12 15:11:22 +0200

    Removed unused file after migration to quasar v1 and moved required code to boot file

--- src/boot/api.js
+++ src/boot/api.js
@@ -1,8 +1,25 @@
 import 'whatwg-fetch'
+import config from 'config'
+import logger from 'loglevel'
+import postRobot from 'post-robot'
+import utils from '../utils'
 import appHooks from '../main.hooks'
 import services from '../services'
 import plugin from '../vue-kdk'
 import { kalisio, beforeGuard, authenticationGuard } from '@kalisio/kdk-core/client'
+
+postRobot.on('setLocalStorage', async (event) => {
+  _.forOwn(event.data, (value, key) => {
+    window.localStorage.setItem(key, (typeof value === 'object' ? JSON.stringify(value) : value))
+  })
+})
+postRobot.on('setConfiguration', async (event) => {
+  _.forOwn(event.data, (value, key) => {
+    _.set(config, key, value)
+  })
+})
+
+utils.sendEmbedEvent('kano-ready')
 
 export default async ({ app, router, Vue }) => {
   let api = kalisio()
