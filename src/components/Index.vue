7d0f851b278a24162e64832a7858832d32ef1515
Luc Claustres <luc.claustres@orange.fr>
2019-09-20 17:34:51 +0200

    Fixed regression on app launch when restoring session

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -87,14 +87,16 @@ export default {
     this.$toast({html: this.$t('Index.VERSION_MISMATCH')})
   },
   async mounted () {
-    try {
-      this.user = await this.restoreSession()
-      // No need to redirect here since the user should be set thus managed by event handler below
-    } catch (_) {
-      this.user = null
-      // Check if we need to redirect based on the fact there is no authenticated user
-      this.redirect()
-    }
+    setTimeout(async () => {
+      try {
+        this.user = await this.restoreSession()
+        // No need to redirect here since the user should be set thus managed by event handler below
+      } catch (_) {
+        this.user = null
+        // Check if we need to redirect based on the fact there is no authenticated user
+        this.redirect()
+      }
+    }, 1000)
     
     this.$events.$on('user-changed', user => {
       this.user = user
