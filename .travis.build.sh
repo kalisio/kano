d1ad0e21d318ac3b48b466f99e94c5bae688ca5e
Christophe Nouguier <christophe.nouguier@free.fr>
2019-12-14 11:49:07 +0100

    ci: cleaned build stage

--- .travis.build.sh
+++ .travis.build.sh
@@ -4,15 +4,6 @@ source .travis.env.sh
 # It first need to create the required network 
 docker network create --attachable $DOCKER_NETWORK
 
-#
-# Build the app
-#
-travis_fold start "build"
-
-# NOTE: The process build the image and run the container in order to allow us to copy the 
-# built artifact from the container to the host. Indeed the artifact is then copied to S3 
-# (see the deploy hook) and can be used by the following stages (i.e. Android and iOS).
-
 # Build the image
 # FIXME: building Kano raises travis error related to the output log => override the
 # output to a file and push it to S3. Be careful if the building process come to take
@@ -35,26 +26,4 @@ if [ $ERROR_CODE -eq 1 ]; then
 	exit 1
 fi
 
-travis_fold end "build"
-
-#
-#  Backup the artifact to S3
-#
-travis_fold start "backup"
-
-# Copy the artifact from the container to the host
-# See https://docs.docker.com/compose/reference/envvars/#compose_project_name
-# explanation on the container name
-docker-compose -f deploy/app.yml up -d
-docker cp ${APP}_app_1:/opt/$APP/dist/spa dist
-
-# Backup the artifact to S3
-aws s3 sync dist s3://$BUILDS_BUCKET/$BUILD_NUMBER/www > /dev/null
-ERROR_CODE=$?
-if [ $ERROR_CODE -eq 1 ]; then
-	echo "Copying the artifact to S3 has failed [error: $ERROR_CODE]"
-	exit 1
-fi
-
-travis_fold end "backup"
 
