fe609c153265148f768107191172c88c87550d50
Christophe Nouguier <christophe.nouguier@free.fr>
2019-12-18 09:48:43 +0100

    wip: Make Travis pipeline more reliable #37

--- .travis.test.sh
+++ .travis.test.sh
@@ -17,10 +17,10 @@ then
 	./cc-test-reporter before-build
 
 	# Run the tests
-	docker-compose -f deploy/mongodb.yml -f deploy/app.yml -f deploy/app.test.server.yml up --exit-code-from app app
+	docker-compose -f deploy/mongodb.yml -f deploy/app.yml -f deploy/app.test.api.yml up --exit-code-from app app
 	ERROR_CODE=$?
 	if [ $ERROR_CODE -ne 0 ]; then
-		echo "Testing ${APP} API failed [error: $ERROR_CODE]"
+		echo "Testing API failed [error: $ERROR_CODE]"
 		exit 1
 	fi
 
@@ -30,11 +30,11 @@ then
 
 	# Report to code climate
 	./cc-test-reporter after-build -t lcov --exit-code $ERROR_CODE
-fi
+else
+  # Run Testcafe with the given fixture 
+	export FIXTURE=$1
 
-if [ $1 == "client" ]
-then
-	Output directory for client screenshots
+	# Create the screenshots dir
 	mkdir screenshots
 	chmod -R 777 screenshots
 
@@ -42,9 +42,9 @@ then
 	docker-compose -f deploy/mongodb.yml -f deploy/app.yml -f deploy/app.test.client.yml up --exit-code-from testcafe testcafe
 	ERROR_CODE=$?
 	#Copy the screenshots whatever the result
-	aws s3 sync screenshots s3://$BUILDS_BUCKET/$BUILD_NUMBER/client-screenshots > /dev/null
+	aws s3 sync screenshots s3://$BUILDS_BUCKET/$BUILD_NUMBER/$FIXTURE-screenshots > /dev/null
 	if [ $ERROR_CODE -eq 1 ]; then
-		echo "Testing ${APP} client failed [error: $ERROR_CODE]"
+		echo "Testing $FIXTURE failed [error: $ERROR_CODE]"
 		exit 1
 	fi
 fi
