4bbf1bed94f6b5a479b0d766a22daf3f955a7670
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-08 17:20:06 +0000

    Deploy kalisio/kano to github.com/kalisio/kano.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "d286c8e6f7cfaa7161f9fcc974e2301a"
+    "revision": "4b06a5d4fbb874b5f18b7342ec9a1e6a"
   },
   {
     "url": "about/contact.html",
