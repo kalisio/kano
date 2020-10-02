1bbe7959fb4dc8077ccb159d7edcdb3dfebb4e5c
Christophe Nouguier <christophe.nouguier@free.fr>
2020-10-02 17:52:48 +0200

    chore: updated layers definition

--- api/config/layers/ign-layers.js
+++ api/config/layers/ign-layers.js
@@ -5,13 +5,13 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
     i18n: {
       fr: {
         Layers: {
-          IGN_ORTHO: 'BD ORTHO IGN',
+          IGN_ORTHO: 'BD ORTHO (IGN)',
           IGN_ORTHO_DESCRIPTION: 'Photos aériennes IGN'
         }
       },
       en: {
         Layers: {
-          IGN_ORTHO: 'IGN BD ORTHO',
+          IGN_ORTHO: 'BD ORTHO (IGN)',
           IGN_ORTHO_DESCRIPTION: 'IGN aerial imagery'
         }
       }
@@ -41,13 +41,13 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
     i18n: {
       fr: {
         Layers: {
-          IGN_SCAN: 'SCAN standard IGN',
+          IGN_SCAN: 'SCAN standard (IGN)',
           IGN_SCAN_DESCRIPTION: 'Cartographie topographique multi-échelles - standard'
         }
       },
       en: {
         Layers: {
-          IGN_SCAN: 'IGN standard SCAN',
+          IGN_SCAN: 'Standard SCAN (IGN)',
           IGN_SCAN_DESCRIPTION: 'Multi-scale topography map - standard'
         }
       }
@@ -77,13 +77,13 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
     i18n: {
       fr: {
         Layers: {
-          IGN_SCAN_CLASSIC: 'SCAN classique IGN',
+          IGN_SCAN_CLASSIC: 'SCAN classique (IGN)',
           IGN_SCAN_CLASSIC_DESCRIPTION: 'Cartographie topographique multi-échelles - classique'
         }
       },
       en: {
         Layers: {
-          IGN_SCAN_CLASSIC: 'IGN classic SCAN',
+          IGN_SCAN_CLASSIC: 'Classic SCAN (IGN)',
           IGN_SCAN_CLASSIC_DESCRIPTION: 'Multi-scale topography map - classic'
         }
       }
