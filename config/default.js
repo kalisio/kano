79f1fdb77b25dac0baeb218b6f4bbb1166185f90
Luc Claustres <luc.claustres@orange.fr>
2021-02-19 19:36:52 +0100

    wip: Fixed layer actions visibility (#154)

--- config/default.js
+++ config/default.js
@@ -401,14 +401,14 @@ module.exports = {
         actionRenderer: 'item',
         content: [
           { id: 'zoom-to', label: 'mixins.activity.ZOOM_TO_LABEL', icon: 'las la-search-location', handler: 'onZoomToLayer' },
-          { id: 'save', label: 'mixins.activity.SAVE_LAYER', icon: 'las la-save', handler: 'onSaveLayer' },
-          { id: 'filter-data', label: 'mixins.activity.FILTER_DATA_LABEL', icon: 'las la-filter', handler: 'onFilterLayerData' },
-          { id: 'view-data', label: 'mixins.activity.VIEW_DATA_LABEL', icon: 'las la-th-list', handler: 'onViewLayerData' },
-          { id: 'chart-data', label: 'mixins.activity.CHART_DATA_LABEL', icon: 'las la-chart-pie', handler: 'onChartLayerData' },
-          { id: 'edit', label: 'mixins.activity.EDIT_LABEL', icon: 'las la-file-alt', handler: 'onEditLayer' },
-          { id: 'edit-style', label: 'mixins.activity.EDIT_LAYER_STYLE_LABEL', icon: 'las la-border-style', handler: 'onEditLayerStyle' },
-          { id: 'edit-data', label: 'mixins.activity.STOP_EDIT_DATA_LABEL', icon: 'las la-edit', handler: 'onEditLayerData' },
-          { id: 'remove', label: 'mixins.activity.REMOVE_LABEL', icon: 'las la-minus-circle', handler: 'onRemoveLayer' }
+          { id: 'save', label: 'mixins.activity.SAVE_LAYER', icon: 'las la-save', handler: 'onSaveLayer', visible: 'isLayerStorable' },
+          { id: 'filter-data', label: 'mixins.activity.FILTER_DATA_LABEL', icon: 'las la-filter', handler: 'onFilterLayerData', visible: 'isLayerEditable' },
+          { id: 'view-data', label: 'mixins.activity.VIEW_DATA_LABEL', icon: 'las la-th-list', handler: 'onViewLayerData', visible: 'isLayerEditable' },
+          { id: 'chart-data', label: 'mixins.activity.CHART_DATA_LABEL', icon: 'las la-chart-pie', handler: 'onChartLayerData', visible: 'isLayerEditable' },
+          { id: 'edit', label: 'mixins.activity.EDIT_LABEL', icon: 'las la-file-alt', handler: 'onEditLayer', visible: 'isLayerEditable' },
+          { id: 'edit-style', label: 'mixins.activity.EDIT_LAYER_STYLE_LABEL', icon: 'las la-border-style', handler: 'onEditLayerStyle', visible: 'isLayerStyleEditable' },
+          { id: 'edit-data', label: 'mixins.activity.STOP_EDIT_DATA_LABEL', icon: 'las la-edit', handler: 'onEditLayerData', visible: 'isLayerEditable' },
+          { id: 'remove', label: 'mixins.activity.REMOVE_LABEL', icon: 'las la-minus-circle', handler: 'onRemoveLayer', visible: 'isLayerRemovable' }
         ]
       }]
     },
