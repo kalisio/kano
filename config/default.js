0d73063434f60970a3bd3809421b53baad92f154
Robin Bourianes <robin.bourianes@frizko.com>
2021-03-22 15:24:37 +0100

    feat: add a component to display WMS legends when available.

--- config/default.js
+++ config/default.js
@@ -405,6 +405,8 @@ module.exports = {
       content: [{
         component: 'frame/KPageSticky', position: 'left', offset: [18, 0], content: [{ component: 'KColorLegend' }]
       }, {
+        component: 'frame/KPageSticky', position: 'top-left', offset: [18, 18], content: [{ component: 'KWmsLegend' }]
+      }, {
         component: 'frame/KPageSticky', position: 'right', offset: [40, 0], content: [{ component: 'KLevelSlider' }]
       } /* Only for example purpose
       {
