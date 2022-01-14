c959245fc4c243a29a6ba7406fa10a671482ee8c
Luc Claustres <luc.claustres@orange.fr>
2022-01-14 18:11:26 +0100

    wip: Updated Little Alert Box layer (#233)

--- api/config/layers/lab-layers.js
+++ api/config/layers/lab-layers.js
@@ -9,7 +9,12 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
           LAB_DESCRIPTION: 'Little Alert Box'
         },
         Variables: {
-          VALUE: 'Valeur mesurée'
+          TEMPERATURE: 'Température',
+          PRESSURE: 'Pression',
+          HUMIDITY: 'Humidité',
+          GAS: 'Gaz',
+          LUMINOSITY: 'Luminosité',
+          ACCELERATION: 'Accélération'
         }
       },
       en: {
@@ -18,7 +23,12 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
           LAB_DESCRIPTION: 'Little Alert Box'
         },
         Variables: {
-          VALUE: 'Measured value'
+          TEMPERATURE: 'Temperature',
+          PRESSURE: 'Pressure',
+          HUMIDITY: 'Humidity',
+          GAS: 'Gas',
+          LUMINOSITY: 'Luminosity',
+          ACCELERATION: 'Acceleration'
         }
       }
     },
@@ -38,17 +48,97 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
     queryFrom: 'PT-1H',
     variables: [
       {
-        name: 'value',
-        label: 'Variables.VALUE',
+        name: 'tmp_ow',
+        label: 'Variables.TEMPERATURE',
         units: [
-          'unit'
+          'degC'
         ],
-        range: [0, 500],
+        range: [-50, 127],
         step: 5,
         chartjs: {
-          backgroundColor: 'rgba(11, 117, 169, 128)',
-          borderColor: 'rgb(11, 117, 169)',
+          backgroundColor: 'rgba(255, 99, 132, 128)',
+          borderColor: 'rgb(255, 99, 132)'
           fill: false
+        }
+      },
+      {
+        name: 'bme_pressure',
+        label: 'Variables.PRESSURE',
+        units: [
+          'hPa'
+        ],
+        range: [-940, 1050],
+        step: 10,
+        chartjs: {
+          backgroundColor: 'rgba(191, 191, 63, 128)',
+          borderColor: 'rgb(191, 191, 63)',
+          fill: false
+        }
+      },
+      {
+        name: 'bme_humidity',
+        label: 'Variables.HUMIDITY',
+        units: [
+          '%'
+        ],
+        range: [0, 100],
+        step: 10,
+        chartjs: {
+          backgroundColor: 'rgba(54, 162, 235, 128)',
+          borderColor: 'rgb(54, 162, 235)',
+          fill: false,
+          yAxis: {
+            ticks: {
+              min: 0
+            }
+          }
+        }
+      },
+      {
+        name: 'bme_gas',
+        label: 'Variables.GAS',
+        units: [
+          'mohm'
+        ],
+        range: [5, 50000],
+        step: 5000,
+        chartjs: {
+          backgroundColor: 'rgba(255, 159, 64, 128)',
+          borderColor: 'rgb(255, 159, 64)',
+          fill: false
+        }
+      },
+      {
+        name: 'luminosity',
+        label: 'Variables.LUMINOSITY',
+        units: [
+          'cd'
+        ],
+        range: [100, 5000],
+        step: 500,
+        chartjs: {
+          backgroundColor: 'rgba(255, 215, 0, 128)',
+          borderColor: 'rgb(255, 215, 0)',
+          fill: false
+        }
+      },
+      {
+        name: 'acc_module',
+        label: 'Variables.ACCELERATION',
+        units: [
+          'G'
+        ],
+        range: [0, 2],
+        step: 0.2,
+        chartjs: {
+          backgroundColor: 'rgba(128, 128, 128, 128)',
+          borderColor: 'rgb(128, 128, 128)',
+          fill: false,
+          yAxis: {
+            ticks: {
+              min: 0
+            }
+          }
         }
       }
     ],
@@ -58,15 +148,15 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
       tiled: true,
       minZoom: 10,
       cluster: { disableClusteringAtZoom: 18 },
-      'marker-color': `<% if (properties.status === 'OK') { %>green<% }
-                          else if (properties.status === 'KO') { %>red<% }
+      'marker-color': `<% if (properties.flag_alert === 0) { %>green<% }
+                          else if (properties.flag_alert === 1) { %>red<% }
                           else { %>dark<% } %>`,
-      'icon-classes': 'fa fa-bell',
+      'icon-classes': 'fa fa-wifi',
       'icon-x-offset': -2,
       'icon-color': '#FFF',
       template: ['marker-color'],
       tooltip: {
-        template: '<%= properties.name %>: <%= properties.status || \'no status\' %><% if (_.has(properties, \'value\')) { %></br><%= properties.value.toFixed(2) %> unit<% } %>'
+        template: '<%= properties.info_bulle %>'
       }
     },
     cesium: {
@@ -76,7 +166,7 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
       'marker-symbol': 'lighthouse',
       'marker-color': '#180EF1',
       tooltip: {
-        template: '<%= properties.name %><% if (_.has(properties, \'value\')) { %>\n<%= properties.value.toFixed(2) %> unit<% } %>'
+        template: '<%= properties.info_bulle %>'
       }
     }
   }]
