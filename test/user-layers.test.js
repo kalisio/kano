bed6b63d401d87548d8811b14694df17903d7777
Karl Azemar <karl.azemar@gmail.com>
2022-04-22 11:51:07 +0200

    Replace old tests

--- test/user-layers.test.js
+++ test/user-layers.test.js
@@ -10,24 +10,26 @@ const userLayersTab = 'user-layers-tab'
 
 describe(`suite:${suite}`, () => {
   let runner, page
-  const user = { email: 'user-kano@kalisio.xyz', password: 'Pass;word1' }
+  const user = [
+    { email: 'user-kano@kalisio.xyz', password: 'Pass;word1' },
+    { email: 'admin-kano@kalisio.xyz', password: 'Pass;word1' }
+  ]
 
   before(async () => {
     chailint(chai, util)
 
     runner = new core.Runner(suite, {
       appName: 'kano',
-      user: user.email,
       geolocation: { latitude: 43.10, longitude: 1.71 },
       localStorage: {
         'kano-welcome': false
       }
     })
     page = await runner.start()
-    await core.login(page, user)
+    await core.login(page, user[0])
   })
 
-  it('drop geojson gradient file', async () => {
+  it('user: drop geojson gradient file', async () => {
     await map.dropFile(page, runner.getDataPath('flight.geojson'))
     const match = await runner.captureAndMatch('flight')
     // await page.screenshot({ path: "./test/data/user-layers/screenrefs/test.png" })
@@ -35,42 +37,42 @@ describe(`suite:${suite}`, () => {
     expect(match).beTrue()
   })
 
-  it('import geojson file with bbox', async () => {
+  it('user: import geojson file with bbox', async () => {
     await map.importLayer(page, runner.getDataPath('landing.geojson'))
     const match = await runner.captureAndMatch('landing')
     await map.clickLayer(page, userLayersTab, 'landing')
     expect(match).beTrue()
   })
 
-  it('import geojson file', async () => {
+  it('user: import geojson file', async () => {
     await map.importLayer(page, runner.getDataPath('departements.geojson'), 'code')
     const match = await runner.captureAndMatch('departements')
     await map.clickLayer(page, userLayersTab, 'departements')
     expect(match).beTrue()
   })
 
-  it('import kml file', async () => {
+  it('user: import kml file', async () => {
     await map.importLayer(page, runner.getDataPath('regions.kml'))
     const match = await runner.captureAndMatch('regions')
     await map.clickLayer(page, userLayersTab, 'regions')
     expect(match).beTrue()
   })
 
-  it('import gpx file', async () => {
+  it('user: import gpx file', async () => {
     await map.importLayer(page, runner.getDataPath('trace.gpx'))
     const match = await runner.captureAndMatch('trace')
     await map.clickLayer(page, userLayersTab, 'trace')
     expect(match).beTrue()
   })
 
-  it('import shp file', async () => {
+  it('user: import shp file', async () => {
     await map.dropFile(page, runner.getDataPath('espaces-naturels.shp'))
     const match = await runner.captureAndMatch('espaces-naturels')
     await map.clickLayer(page, userLayersTab, 'espaces-naturels')
     expect(match).beTrue()
   })
 
-  it('connect wms layer', async () => {
+  it('user: connect wms layer', async () => {
     const service = 'https://geoservices.brgm.fr/geologie?service=wms&request=getcapabilities'
     const layerId = 'geologie'
     await map.connectLayer(page, service, layerId)
@@ -79,40 +81,20 @@ describe(`suite:${suite}`, () => {
     expect(match).beTrue()
   })
 
-  after(async () => {
+  it('switch to admin', async () => {
     await core.logout(page)
-    await runner.stop()
-  })
-})
-
-// Connecting with admin account : testing dropFile, captureAndMatch and removeLayer
-describe(`suite:${suite}`, () => {
-  let runner, page
-  const user = { email: 'admin-kano@kalisio.xyz', password: 'Pass;word1' }
-
-  before(async () => {
-    chailint(chai, util)
-
-    runner = new core.Runner(suite, {
-      appName: 'kano',
-      user: user.email,
-      geolocation: { latitude: 43.10, longitude: 1.71 },
-      localStorage: {
-        'kano-welcome': false
-      }
-    })
-    page = await runner.start()
-    await core.login(page, user)
+    await core.goToLoginScreen(page)
+    await core.login(page, user[1])
   })
 
-  it('add polygon mask', async () => {
+  it('admin: add polygon mask', async () => {
     await map.dropFile(page, runner.getDataPath('ariege.geojson'))
     const match = await runner.captureAndMatch('polygon-mask')
     expect(match).beTrue()
     await map.removeLayer(page, userLayersTab, 'ariege')
   })
 
-  it('add multi-polygon mask', async () => {
+  it('admin: add multi-polygon mask', async () => {
     await map.dropFile(page, runner.getDataPath('occitanie.geojson'))
     const match = await runner.captureAndMatch('multi-polygon-mask')
     expect(match).beTrue()
--- test/user-views.test.js
+++ test/user-views.test.js
@@ -11,7 +11,10 @@ const catalogLayersTab = 'catalog-layers-tab'
 
 describe(`suite:${suite}`, () => {
   let runner, page
-  const user = { email: 'admin-kano@kalisio.xyz', password: 'Pass;word1' }
+  const user = [
+    { email: 'user-kano@kalisio.xyz', password: 'Pass;word1' },
+    { email: 'admin-kano@kalisio.xyz', password: 'Pass;word1' }
+  ]
 
   before(async () => {
     chailint(chai, util)
@@ -21,38 +24,60 @@ describe(`suite:${suite}`, () => {
       geolocation: { latitude: 43.10, longitude: 1.71 },
       localStorage: {
         'kano-welcome': false
-      },
-      mode: 'screenshots'
+      }
     })
     page = await runner.start()
-    await core.login(page, user)
+    await core.login(page, user[1])
   })
 
-  it('create views', async () => {
-    const andorraExtent = [42.3915, 1.2847, 42.7051, 1.9315]
-    const toulouseExtent = [43.5895, 1.41584, 43.6087, 1.4562]
-    await map.zoomToExtent(page, andorraExtent)
+  it('admin: create Andorra view', async () => {
+    const extent = [42.3915, 1.2847, 42.7051, 1.9315]
+    await map.zoomToExtent(page, extent)
     await map.createView(page, 'Andorra', false)
-    await map.zoomToExtent(page, toulouseExtent)
+    expect(await map.viewExists(page, userViewsTab, 'Andorra')).beTrue()
+  })
+
+  it('admin: create Toulouse view', async () => {
+    const extent = [43.5895, 1.41584, 43.6087, 1.4562]
+    await map.zoomToExtent(page, extent)
     await map.clickLayer(page, catalogLayersTab, 'HYBRID')
     await map.createView(page, 'Toulouse', true)
-    expect(await map.viewExists(page, userViewsTab, 'Andorra')).beTrue()
     expect(await map.viewExists(page, userViewsTab, 'Toulouse')).beTrue()
   })
 
-  it('restore andorra', async () => {
+  it('switch to user', async () => {
+    await core.logout(page)
+    await core.goToLoginScreen(page)
+    await core.login(page, user[0])
+  })
+
+  it('user: restore andorra view', async () => {
     await map.clickView(page, userViewsTab, 'Andorra')
     const match = await runner.captureAndMatch('andorra')
     expect(match).beTrue()
   })
 
-  it('restore toulouse', async () => {
+  it('user: restore toulouse view', async () => {
     await map.clickView(page, userViewsTab, 'Toulouse')
     const match = await runner.captureAndMatch('toulouse')
     expect(match).beTrue()
   })
 
-  it('remove views', async () => {
+  it('user: can\'t create views', async () => {
+    expect(await core.elementExists(page, 'create-view')).beFalse()
+  })
+
+  it('user: can\'t remove views', async () => {
+    expect(await core.elementExists(page, 'remove-view')).beFalse()
+  })
+
+  it('switch to admin', async () => {
+    await core.logout(page)
+    await core.goToLoginScreen(page)
+    await core.login(page, user[1])
+  })
+
+  it('admin: remove views', async () => {
     await map.removeView(page, userViewsTab, 'Andorra')
     await map.removeView(page, userViewsTab, 'Toulouse')
     expect(await map.viewExists(page, userViewsTab, 'Andorra')).beFalse()
