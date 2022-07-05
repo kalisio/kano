bc531edb191d031d2f9e00a2b186e443036c8b57
Luc Claustres <luc.claustres@orange.fr>
2022-07-05 23:04:17 +0200

    chore: used categories/layers configuration functions now in KDK

--- api/config/layers.cjs
+++ api/config/layers.cjs
@@ -1,7 +1,6 @@
-const path = require('path')
 const glob = require('glob')
-const makeDebug = require('debug')
-const debug = makeDebug('kano:layers')
+const path = require('path')
+const { getLayers } = require('@kalisio/kdk/map.config.cjs')
 
 // Override defaults if env provided
 const kargoDomain = (process.env.SUBDOMAIN ? process.env.SUBDOMAIN : 'test.kalisio.xyz')
@@ -12,44 +11,4 @@ const wcsUrl = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/wc
 const k2Url = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/k2' : 'https://k2.' + kargoDomain)
 const s3Url = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/s3' : 'https://s3.eu-central-1.amazonaws.com')
 
-// Request layer definition files
-const layerFiles = glob.sync(path.join(__dirname, 'layers/**/*.cjs'))
-debug('Processing the following layer definition files to build catalog from', layerFiles)
-
-// Process them
-let layers = []
-layerFiles.forEach(layerFile => {
-  let layersFromFile
-  try {
-    layersFromFile = require(layerFile)
-  } catch (error) {
-    console.error(error)
-  }
-  // Layers provided through a generation function ?
-  if (typeof layersFromFile === 'function') layersFromFile = layersFromFile({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url })
-  // Layers directly provided as array or object
-  else if (!Array.isArray(layersFromFile)) layersFromFile = [layersFromFile]
-  layers = layers.concat(layersFromFile)
-})
-debug(`Found ${layers.length} layer definitions to build catalog from`)
-
-// No layers by default
-let filter = []
-// Now build filter according any env filter
-if (process.env.LAYERS_FILTER) {
-  // Check for wildcard to get all layers
-  if (process.env.LAYERS_FILTER === '*') filter = layers.map(layer => layer.name.replace('Layers.', ''))
-  // Check for list with separator, whitespace or comma is supported
-  else if (process.env.LAYERS_FILTER.includes(',')) filter = process.env.LAYERS_FILTER.split(',')
-  else filter = process.env.LAYERS_FILTER.split(' ')
-}
-// Now filter layers
-// Manage translation keys starting with 'Layers.'
-debug('Applying layer filter', filter)
-layers = layers.filter(layer => {
-  const isFiltered = !filter.includes(layer.name.replace('Layers.', ''))
-  if (isFiltered) debug(`Filtering ${layer.name} from catalog`)
-  return !isFiltered
-})
-
-module.exports = layers
+module.exports = getLayers(glob.sync(path.join(__dirname, 'layers/**/*.cjs')), { wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url })
