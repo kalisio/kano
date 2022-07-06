f75abcb114e6fa3f456bf44848af6d34c98b02e6
Luc Claustres <luc.claustres@orange.fr>
2022-07-06 18:05:26 +0200

    chore: used default users/catalog layers creation functions now in KDK

--- api/src/services.js
+++ api/src/services.js
@@ -1,11 +1,9 @@
-import _ from 'lodash'
 import path from 'path'
 import fs from 'fs-extra'
-import zlib from 'zlib'
 import request from 'superagent'
 import { fileURLToPath } from 'url'
-import kCore, { permissions } from '@kalisio/kdk/core.api.js'
-import kMap, { createFeaturesService } from '@kalisio/kdk/map.api.js'
+import kCore, { permissions, createDefaultUsers } from '@kalisio/kdk/core.api.js'
+import kMap, { createFeaturesServiceForLayer, createDefaultCatalogLayers } from '@kalisio/kdk/map.api.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -61,114 +59,11 @@ export default async function () {
     app.logger.error(error.message)
   }
 
-  const defaultUsers = app.get('authentication').defaultUsers
-  // Do not use exposed passwords on staging/prod environments
-  if (defaultUsers) {
-    const usersService = app.getService('users')
-    // Create default users if not already done
-    const users = await usersService.find({ paginate: false })
-    for (let i = 0; i < defaultUsers.length; i++) {
-      const defaultUser = defaultUsers[i]
-      const createdUser = _.find(users, { email: defaultUser.email })
-      if (!createdUser) {
-        app.logger.info('Initializing default user (email = ' + defaultUser.email + ')')
-        await usersService.create(defaultUser)
-      }
-    }
-  }
-
-  // Helper to register service and permissions for a layer
-  async function createFeaturesServiceForLayer (options) {
-    const service = await createFeaturesService.call(app, options)
-    // Register default permissions for it
-    permissions.defineAbilities.registerHook((subject, can, cannot) => {
-      can('service', options.collection)
-      can('read', options.collection)
-    })
-    return service
-  }
-
-  const catalogService = app.getService('catalog')
-  const catalog = app.get('catalog')
-
-  const defaultLayers = catalog ? catalog.layers || [] : []
-  const layers = await catalogService.find({ query: {}, paginate: false })
-  for (let i = 0; i < defaultLayers.length; i++) {
-    const defaultLayer = defaultLayers[i]
-    const createdLayer = _.find(layers, { name: defaultLayer.name })
-    try {
-      if (!createdLayer) {
-        app.logger.info('Adding default layer (name = ' + defaultLayer.name + ')')
-        await catalogService.create(defaultLayer)
-      } else {
-        app.logger.info('Updating default layer (name = ' + defaultLayer.name + ')')
-        await catalogService.update(createdLayer._id, defaultLayer)
-      }
-    } catch (error) {
-      console.error(error)
-    }
-    // Check if service(s) are associated to this layer
-    let featuresService
-    if (defaultLayer.service) {
-      featuresService = app.getService(defaultLayer.service)
-      // Avoid create it twice as we can share services between different layers
-      if (featuresService) continue
-      featuresService = await createFeaturesServiceForLayer({
-        collection: defaultLayer.service,
-        ttl: defaultLayer.ttl,
-        featureId: defaultLayer.featureId,
-        featureIdType: defaultLayer.featureIdType,
-        db: app.db.db(defaultLayer.dbName)
-      })
-    }
-    if (defaultLayer.probeService) {
-      await createFeaturesServiceForLayer({
-        collection: defaultLayer.probeService,
-        db: app.db.db(defaultLayer.dbName)
-      })
-    }
-    // And if we need to initialize some data as well
-    if (!createdLayer && featuresService && (defaultLayer.url || defaultLayer.fileName)) {
-      // Cleanup
-      try {
-        await featuresService.remove(null, { query: {} })
-      } catch (error) {
-        console.error(error)
-      }
-      // Data requested from external service ?
-      if (defaultLayer.url) {
-        try {
-          const response = await request.get(defaultLayer.url)
-          await featuresService.create(response.body.features)
-        } catch (error) {
-          console.error(error)
-        }
-      } else if (path.extname(defaultLayer.fileName) === '.gz') {
-        const extractedFileName = path.join(path.dirname(defaultLayer.fileName), path.basename(defaultLayer.fileName, '.gz'))
-        fs.createReadStream(defaultLayer.fileName)
-          .pipe(zlib.createGunzip())
-          .pipe(fs.createWriteStream(extractedFileName))
-          .on('close', async () => {
-            const geojson = fs.readJsonSync(extractedFileName)
-            try {
-              await featuresService.create(geojson.features)
-            } catch (error) {
-              console.error(error)
-            }
-          })
-          .on('error', (error) => { console.log(error) })
-      } else {
-        const geojson = fs.readJsonSync(defaultLayer.fileName)
-        try {
-          await featuresService.create(geojson.features)
-        } catch (error) {
-          console.error(error)
-        }
-      }
-    }
-  }
+  // Initialize defaults
+  await createDefaultUsers.call(app)
+  await createDefaultCatalogLayers.call(app)
 
   // Service to store user features
-  const featuresService = await createFeaturesServiceForLayer({ collection: 'features' })
+  const featuresService = await createFeaturesServiceForLayer.call(app, { collection: 'features' })
   await app.configureService('features', featuresService, servicesPath)
 }
