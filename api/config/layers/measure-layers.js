9bc94e51a8d954d29678e81645d8d3413a721644
Luc Claustres <luc.claustres@orange.fr>
2023-01-04 18:45:40 +0100

    feat: Add layer configuration for ICOS atmospheric data (closes #301)

--- api/config/layers/measure-layers.js
+++ api/config/layers/measure-layers.js
@@ -662,5 +662,163 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
                   'if (feature.time && feature.time.value) { %></br><%= Time.format(feature.time.value, \'time.long\') + \' - \' + Time.format(feature.time.value, \'date.short\') %><% } %>'
       }
     }
+  },
+  {
+    name: 'Layers.ICOS',
+    description: 'Layers.ICOS_DESCRIPTION',
+    i18n: {
+      fr: {
+        Layers: {
+          ICOS: 'ICOS',
+          ICOS_DESCRIPTION: 'Integrated Carbon Observation System'
+        },
+        Variables: {
+          RADON: 'Radon',
+          CO: 'Monoxyde de carbone (CO)',
+          CO2: 'Dioxyde de carbone (CO²)',
+          CH4: 'Méthane (CH4)'
+        }
+      },
+      en: {
+        Layers: {
+          ICOS: 'ICOS',
+          ICOS_DESCRIPTION: 'Integrated Carbon Observation System'
+        },
+        Variables: {
+          RADON: 'Radon',
+          CO: 'Carbon monoxide (CO)',
+          CO2: 'Carbon dioxide (CO²)',
+          CH4: 'Methane (CH4)'
+        }
+      }
+    },
+    tags: [
+      'atmospheric', 'measure'
+    ],
+    iconUrl: 'https://s3.eu-central-1.amazonaws.com/kalisioscope/assets/icos-icon.png',
+    attribution: '',
+    type: 'OverlayLayer',
+    service: 'icos-observations',
+    dbName: (process.env.DATA_DB_URL ? 'data' : undefined),
+    probeService: 'icos-stations',
+    featureId: 'stationId',
+    from: 'P-7D',
+    to: 'PT-1H',
+    every: 'PT1H',
+    queryFrom: 'PT-24H',
+    variables: [
+      {
+        name: 'rn',
+        label: 'Variables.RADON',
+        units: [
+          'Bq/m3'
+        ],
+        range: [0, 500],
+        step: 10,
+        chartjs: {
+          backgroundColor: 'rgba(11, 117, 169, 128)',
+          borderColor: 'rgb(11, 117, 169)',
+          fill: false
+        }
+      },
+      {
+        name: 'co',
+        label: 'Variables.CO',
+        units: [
+          'ppm'
+        ],
+        range: [0, 10000],
+        step: 100,
+        chartjs: {
+          backgroundColor: 'rgba(255, 99, 132, 128)',
+          borderColor: 'rgb(255, 99, 132)',
+          fill: false
+        }
+      },
+      {
+        name: 'co2',
+        label: 'Variables.CO2',
+        units: [
+          'ppm'
+        ],
+        range: [0, 10000],
+        step: 100,
+        chartjs: {
+          backgroundColor: 'rgba(81, 186, 153, 128)',
+          borderColor: 'rgb(81, 186, 153)',
+          fill: false
+        }
+      },
+      {
+        name: 'ch4',
+        label: 'Variables.CH4',
+        units: [
+          'ppm'
+        ],
+        range: [0, 10000],
+        step: 100,
+        chartjs: {
+          backgroundColor: 'rgba(40, 44, 32, 128)',
+          borderColor: 'rgb(40, 44, 32)',
+          fill: false
+        }
+      }
+    ],
+    leaflet: {
+      type: 'geoJson',
+      realtime: true,
+      tiled: true,
+      minZoom: 5,
+      minFeatureZoom: 7,
+      cluster: { disableClusteringAtZoom: 18 },
+      'marker-color': `<% if (_.has(properties, 'rn') ||
+                              _.has(properties, 'co') ||
+                              _.has(properties, 'co2') ||
+                              _.has(properties, 'ch4')) { %>#138dce<% }
+                          else if (feature.measureRequestIssued) { %>orange<% }
+                          else { %>grey<% } %>`,
+      'icon-classes': 'fas fa-ellipsis-v',
+      'icon-x-offset': 4,
+      'icon-y-offset': 2,
+      'icon-color': '#FFF',
+      template: ['marker-color'],
+      popup: {
+        pick: [
+          'stationName'
+        ]
+      },
+      tooltip: {
+        template: `<% if (_.has(properties, 'rn')) { %>RN = <%= Units.format(properties.rn, 'Bq/m3') %></br>
+                   <%= Time.format(feature.time.rn, 'time.long') + ' - ' + Time.format(feature.time.rn, 'date.short') %></br><% } %>
+                   <% if (_.has(properties, 'co')) { %>CO = <%= Units.format(properties.co, 'ppm') %></br>
+                   <%= Time.format(feature.time.co, 'time.long') + ' - ' + Time.format(feature.time.co, 'date.short') %></br><% } %>
+                   <% if (_.has(properties, 'co2')) { %>CO2 = <%= Units.format(properties.co2, 'ppm') %></br>
+                   <%= Time.format(feature.time.co2, 'time.long') + ' - ' + Time.format(feature.time.co2, 'date.short') %></br><% } %>
+                   <% if (_.has(properties, 'ch4')) { %>CH4 =<%= Units.format(properties.ch4, 'ppm') %></br>
+                   <%= Time.format(feature.time.ch4, 'time.long') + ' - ' + Time.format(feature.time.ch4, 'date.short') %></br><% } %>`
+      }
+    },
+    cesium: {
+      type: 'geoJson',
+      realtime: true,
+      cluster: { pixelRange: 50 },
+      'marker-symbol': 'lighthouse',
+      'marker-color': '#180EF1',
+      popup: {
+        pick: [
+          'stationName'
+        ]
+      },
+      tooltip: {
+        template: '<% if (_.has(properties, \'rn\')) { %>RN = <%= Units.format(properties.rn, \'Bq/m3\') %>\n' +
+                  '<%= Time.format(feature.time.rn, \'time.long\') + \' - \' + Time.format(feature.time.rn, \'date.short\') %>\n<% } %>' +
+                  '<% if (_.has(properties, \'co\')) { %>CO = <%= Units.format(properties.co, \'ppm\') %>\n' +
+                  '<%= Time.format(feature.time.co, \'time.long\') + \' - \' + Time.format(feature.time.co, \'date.short\') %>\n<% } %>' +
+                  '<% if (_.has(properties, \'co2\')) { %>CO2 = <%= Units.format(properties.co2, \'ppm\') %>\n' +
+                  '<%= Time.format(feature.time.co2, \'time.long\') + \' - \' + Time.format(feature.time.co2, \'date.short\') %>\n<% } %>' +
+                  '<% if (_.has(properties, \'ch4\')) { %>CH4 =<%= Units.format(properties.ch4, \'ppm\') %>\n' +
+                  '<%= Time.format(feature.time.ch4, \'time.long\') + \' - \' + Time.format(feature.time.ch4, \'date.short\') %>\n<% } %>'
+      }
+    }
   }]
 }
