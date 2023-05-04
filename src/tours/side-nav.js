62d10b64f3bb2def977f31c16c35f1ec687c238c
Tristan Greffe <tristan.greffe@hotmail.com>
2023-05-04 16:24:44 +0200

    chore: use generic tour side-nav for code optimization

--- src/tours/side-nav.js
+++ src/tours/side-nav.js
@@ -1,12 +1,8 @@
-module.exports = [{
-  target: '#logout',
-  title: 'tours.side-nav.LOGOUT_LABEL',
-  content: 'tours.side-nav.SESSION_LABEL',
-  params: {
-    placement: 'top',
-    clickOnNext: '#left-opener',
-  }
-}, {
+const _ = require('lodash')
+const tour = require('./map/side-nav')
+
+module.exports = [
+  _.find(tour, { target: '#logout' }), {
   target: '#edit-settings',
   content: 'tours.side-nav.SETTINGS_LABEL',
   params: {
@@ -15,39 +11,8 @@ module.exports = [{
     clickOnNext: '#left-opener',
     previousDelay: 500
   }
-}, {
-  target: '#online-help',
-  link: 'tours.side-nav.HELP_LABEL',
-  params: {
-    placement: 'top',
-    clickOnPrevious: '#left-opener',
-    clickOnNext: '#left-opener',
-    clickOnLink: '#online-help'
-  }
-}, {
-  target: '#contextual-help',
-  content: 'tours.side-nav.CONTEXT_HELP_LABEL',
-  params: {
-    placement: 'top',
-    clickOnPrevious: '#left-opener',
-    clickOnNext: '#left-opener'
-  }
-}, {
-  target: '#about',
-  content: 'tours.side-nav.ABOUT_LABEL',
-  params: {
-    placement: 'top',
-    clickOnPrevious: '#left-opener',
-    clickOnNext: ['#left-opener', '#about'],
-    nextDelay: 1000
-  }
-}, {
-  target: '#report-bug',
-  content: 'tours.side-nav.BUG_LABEL',
-  params: {
-    placement: 'top',
-    clickOnPrevious: ['#ok-action', '#left-opener'],
-    clickOnNext: '#ok-action',
-    previousDelay: 500
-  }
-}]
+}, 
+_.find(tour, { target: '#contextual-help' }), 
+_.find(tour, { target: '#about' }), 
+_.find(tour, { target: '#report-bug' })
+]
