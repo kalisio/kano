b2dd4226b93305b347fe693c7306b3ef461b6da1
Tristan Greffe <tristan.greffe@hotmail.com>
2023-07-12 17:17:09 +0200

    chore: improve service worker #319

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -1,9 +1,3 @@
-/*
- * This file (which will be your service worker)
- * is picked up by the build system ONLY if
- * quasar.config.js > pwa > workboxPluginMode is set to "InjectManifest"
- */
-
 import { precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
 import { NetworkFirst } from 'workbox-strategies'
@@ -12,17 +6,15 @@ import { NetworkFirst } from 'workbox-strategies'
 self.__WB_DISABLE_DEV_LOGS = true
 
 // Activate new service worker
-self.addEventListener('message', (event) => {
+self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting()
   }
 })
 
 // Caching for offline mode
-
 // Preload and cache all resources defined in the manifest
 precacheAndRoute(self.__WB_MANIFEST)
-
 // Register the `NetworkFirst` caching strategy for all HTTP requests
 registerRoute(
   ({url}) => url.href.startsWith('http'),
