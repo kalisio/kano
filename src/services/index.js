b3bbd4cdf9a788d7b40680f54cde2681541625a7
Luc Claustres <luc.claustres@orange.fr>
2023-09-13 11:58:50 +0200

    refactor: updated code for client module initialization functions that are now async in KDK

--- src/services/index.js
+++ src/services/index.js
@@ -2,13 +2,13 @@ import logger from 'loglevel'
 import kdkCore from '@kalisio/kdk/core.client'
 import kdkMap from '@kalisio/kdk/map.client.map'
 
-export default function () {
+export default async function () {
   const api = this
 
   // Set up our plugin services
   try {
-    api.configure(kdkCore)
-    api.configure(kdkMap)
+    await api.configure(kdkCore)
+    await api.configure(kdkMap)
     // Restore previous settings if any
     const settingsService = api.getService('settings')
     if (settingsService) settingsService.restoreSettings()
