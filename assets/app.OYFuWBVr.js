85cb1bb065d6d89b6b186357b2f9294358fa4487
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2024-02-02 14:34:51 +0000

    Deploy kalisio/kano to github.com/kalisio/kano.git:gh-pages

--- assets/app.OYFuWBVr.js
+++
@@ -1,7 +0,0 @@
-import{j as o,ak as i,al as u,am as l,an as c,ao as f,ap as d,aq as m,ar as h,as as A,at as g,aj as v,d as P,u as w,l as y,z as C,au as _,av as E,aw as R,a2 as b}from"./chunks/framework.J61Sinzt.js";import{t as j}from"./chunks/theme.VIFLNX3K.js";function p(e){if(e.extends){const a=p(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const s=p(j),D=P({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=w();return y(()=>{C(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&_(),E(),R(),s.setup&&s.setup(),()=>b(s.Layout)}});async function L(){const e=S(),a=O();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),s.enhanceApp&&await s.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function O(){return h(D)}function S(){let e=o,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),o&&(e=!1),r},s.NotFound)}o&&L().then(({app:e,router:a,data:t})=>{a.go().then(()=>{i(a.route,t.site),e.mount("#app")})});export{L as createApp};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
