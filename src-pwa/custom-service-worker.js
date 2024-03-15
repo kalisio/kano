5aff89edd08db31fdc82e798d6b277aed67f5541
Luc Claustres <luc.claustres@orange.fr>
2024-03-15 09:44:23 +0100

    wip: Allow to make layers available offline - initiated cache layer feature (#353)

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -1,7 +1,11 @@
 import { precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
-import { NetworkFirst } from 'workbox-strategies'
+import { NetworkFirst, CacheFirst } from 'workbox-strategies'
+import { ExpirationPlugin } from 'workbox-expiration'
+import logger from 'loglevel'
+import localforage from 'localforage'
 
+let cachedUrls
 // Disable workbox logs 
 self.__WB_DISABLE_DEV_LOGS = true
 
@@ -12,11 +16,45 @@ self.addEventListener('message', event => {
   }
 })
 
+// As route function cannot be async and localforage is we retrieve
+// the cached layer list on a reccurent basis to be able to respond synchronously
+setInterval(async () => {
+  cachedUrls = await localforage.getItem('layers')
+}, 1000)
+
 // Caching for offline mode
 // Preload and cache all resources defined in the manifest
 precacheAndRoute(self.__WB_MANIFEST)
+// Register the `NetworkFirst` caching strategy for offline data
+// targetting 
+registerRoute(
+  ({url}) => {
+    const isCached = cachedUrls && cachedUrls[url.href]
+    if (isCached) {
+      logger.debug(`[Kano] Return response for ${url.href} from layers cache`)
+    }
+    return isCached
+  },
+  new NetworkFirst({
+    cacheName: 'layers'
+  })
+)
 // Register the `NetworkFirst` caching strategy for all HTTP requests
 registerRoute(
-  ({url}) => url.href.startsWith('http'),
-  new NetworkFirst()
+  ({url}) => {
+    return url.href.startsWith('http')
+  },
+  new NetworkFirst({
+    cacheName: 'app',
+    plugins: [
+      new ExpirationPlugin({
+        // Keep at most N entries.
+        maxEntries: 1000000,
+        // Don't keep any entries for more than N days.
+        maxAgeSeconds: 30 * (24 * 60 * 60),
+        // Automatically cleanup if quota is exceeded.
+        purgeOnQuotaError: true
+      })
+    ]
+  })
 )
