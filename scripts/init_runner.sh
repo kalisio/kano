82e069475d22c79de5f9f758ee7268b08ee21951
Robin Bourianes <robin.bourianes@kalisio.com>
2024-05-13 10:42:09 +0200

    wip: forgot build_docs requirements [skip app, build docs]

--- scripts/init_runner.sh
+++ scripts/init_runner.sh
@@ -16,6 +16,10 @@ init_github_build_app() {
     install_reqs age sops nvm node16
 }
 
+init_github_build_docs() {
+    install_reqs age sops nvm node18
+}
+
 begin_group "Init $CI_ID for $JOB_ID"
 
 init_"${CI_ID}_${JOB_ID}"
