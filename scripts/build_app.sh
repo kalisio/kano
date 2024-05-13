de5ab850664ef8d15607e8cd8dd023acd7b611bc
Robin Bourianes <robin.bourianes@kalisio.com>
2024-05-13 10:32:29 +0200

    wip: using kash for ci

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,76 +1,78 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
+# set -x
 
 THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
-THIS_PATH=$(dirname "$THIS_FILE")
-ROOT_PATH=$(dirname "$THIS_PATH")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
 
-. "$THIS_PATH/kash/kash.sh"
+. "$THIS_DIR/kash/kash.sh"
 
-use_node 16
+## Parse options
+##
 
-# APP=$(node -p -e "require('./package.json').name")
-# NOTE: since kano is not renamed yet
-APP=maps
-VERSION=$(node -p -e "require('./package.json').version")
+PUBLISH=false
+CI_STEP_NAME="Build app"
+while getopts "pr:" option; do
+    case $option in
+        p) # define to publish container to registry
+            PUBLISH=true
+            ;;
+        r) # report outcome to slack
+            CI_STEP_NAME=$OPTARG
+            load_env_files "$WORKSPACE_DIR/development/common/SLACK_WEBHOOK_APPS.enc.env"
+            trap 'slack_ci_report "$ROOT_DIR" "$CI_STEP_NAME" "$?" "$SLACK_WEBHOOK_APPS"' EXIT
+            ;;
+        *)
+            ;;
+    esac
+done
 
-# MAJOR=$(grep "^([0-9]+)\..*")
-# MINOR=$(grep "^([0-9]+)\..*")
+## Init workspace
+##
 
-GIT_TAG=$(get_git_tag)
-GIT_BRANCH=$(get_git_branch)
+init_app_infos "$ROOT_DIR" "$WORKSPACE_DIR/development/workspaces/apps"
 
-BUILD_FLAVOR=dev
-KLI_FILE="$APP"
-if [[ "$GIT_TAG" =~ ^prod-v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-    BUILD_FLAVOR=prod
-    KLI_FILE="$APP-$VERSION"
-elif [[ "$GIT_BRANCH" =~ ^test-*|-test$ ]]; then
-    BUILD_FLAVOR=test
-    KLI_FILE="$APP-$MAJOR.$MINOR"
+APP=$(get_app_name)
+VERSION=$(get_app_version)
+FLAVOR=$(get_app_flavor)
+
+echo "About to build $APP v$VERSION-$FLAVOR ..."
+
+load_env_files "$WORKSPACE_DIR/development/common/kalisio_dockerhub.enc.env"
+load_value_files "$WORKSPACE_DIR/development/common/KALISIO_DOCKERHUB_PASSWORD.enc.value"
+
+## Build container
+##
+
+# kli file is used in container to install, link
+KLI_FILE=$(get_app_kli_file)
+cp "$KLI_FILE" "$WORKSPACE_DIR/kli.js"
+
+echo "Will use kli file $KLI_FILE to install and link modules ..."
+
+IMAGE_NAME="kalisio/$APP"
+IMAGE_TAG="$VERSION-$FLAVOR"
+
+begin_group "Building container ..."
+
+docker login --username "$KALISIO_DOCKERHUB_USERNAME" --password-stdin < "$KALISIO_DOCKERHUB_PASSWORD"
+# DOCKER_BUILDKIT is here to be able to use Dockerfile specific dockerginore (app.Dockerfile.dockerignore)
+DOCKER_BUILDKIT=1 docker build \
+    --build-arg APP="$APP" \
+    --build-arg FLAVOR="$FLAVOR" \
+    --build-arg BUILD_NUMBER="$(get_git_commit_short_sha "$ROOT_DIR")" \
+    -f app.Dockerfile \
+    -t "$IMAGE_NAME:$IMAGE_TAG" \
+    "$WORKSPACE_DIR"
+docker tag "$IMAGE_NAME:$IMAGE_TAG" "$IMAGE_NAME:$FLAVOR"
+
+if [ "$PUBLISH" = true ]; then
+    docker push "$IMAGE_NAME:$IMAGE_TAG"
+    docker push "$IMAGE_NAME:$FLAVOR"
 fi
 
-# CI needs to clone development repo
-if [ "$CI" = true ]; then
-    begin_group "Fetching project dependencies ..."
+docker logout
 
-    KALISIO_DEVELOPMENT_DIR="$TMP_PATH/kalisio"
-    mkdir -p "$KALISIO_DEVELOPMENT_DIR"
-
-    # clone developement into $KALISIO_DEVELOPMENT_DIR
-    git clone --depth 1 "https://$GITHUB_DEVELOPMENT_PAT@github.com/kalisio/development.git" "$KALISIO_DEVELOPMENT_DIR/development"
-
-    # kli and dependencies
-    git clone --depth 1 https://github.com/kalisio/kli.git "$KALISIO_DEVELOPMENT_DIR/kli"
-    cd "$KALISIO_DEVELOPMENT_DIR/kli" && yarn install
-
-    if [ -f "$KALISIO_DEVELOPMENT_DIR/development/workspaces/apps/$APP/$BUILD_FLAVOR/$KLI_FILE-$GIT_BRANCH.js" ]; then
-        KLI_FILE="$KALISIO_DEVELOPMENT_DIR/development/workspaces/apps/$APP/$BUILD_FLAVOR/$KLI_FILE-$GIT_BRANCH.js"
-    else
-        KLI_FILE="$KALISIO_DEVELOPMENT_DIR/development/workspaces/apps/$APP/$BUILD_FLAVOR/$KLI_FILE.js"
-    fi
-
-    node "$KALISIO_DEVELOPMENT_DIR/kli" "$KLI_FILE" --clone --shallow-clone
-    node "$KALISIO_DEVELOPMENT_DIR/kli" "$KLI_FILE" --install
-    node "$KALISIO_DEVELOPMENT_DIR/kli" "$KLI_FILE" --link
-
-    end_group "Fetching project dependencies ..."
-fi
-
-# ## Load project env
-# ##
-
-# . "$KALISIO_DEVELOPMENT_DIR/development/workspaces/tools/tools.sh" docker-bdortho
-
-# ### Build and push container
-# ###
-
-# IMAGE_NAME="$KALISIO_HARBOR_URL/kalisio/docker-bdortho"
-# IMAGE_TAG="latest"
-
-# docker build -t "$IMAGE_NAME:$IMAGE_TAG" "$ROOT_PATH"
-
-# docker login --username "$KALISIO_HARBOR_USERNAME" --password-stdin "$KALISIO_HARBOR_URL" < "$KALISIO_HARBOR_PASSWORD"
-# docker push "$IMAGE_NAME:$IMAGE_TAG"
-# docker logout "$KALISIO_HARBOR_URL"
+end_group "Building container ..."
---
+++ scripts/setup_workspace.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+
+. "$THIS_DIR/kash/kash.sh"
+
+## Parse options
+##
+
+WORKSPACE_BRANCH=
+WORKSPACE_TAG=
+WORKSPACE_NODE=16
+WORKSPACE_KIND=klifull
+OPT_LIST="n:k:"
+if [ "$CI" != true ]; then
+    OPT_LIST="b:n:t:k:"
+fi
+
+while getopts "$OPT_LIST" OPT; do
+    case $OPT in
+        b) # defines branch to pull
+            WORKSPACE_BRANCH=$OPTARG;;
+        n) # defines node version
+            WORKSPACE_NODE=$OPTARG;;
+        t) # defines tag to pull
+            WORKSPACE_TAG=$OPTARG;;
+        k) # workspace kind (nokli kli klifull)
+            WORKSPACE_KIND=$OPTARG;;
+        *)
+        ;;
+    esac
+done
+
+begin_group "Setting up workspace ..."
+
+if [ "$CI" = true ]; then
+    WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
+    # workaround since repo is kApp with a 'A' and in kli file it's kapp with a 'a'
+    # mv "$WORKSPACE_DIR/kApp" "$WORKSPACE_DIR/kapp"
+    # ln -s "$WORKSPACE_DIR/kapp" "$WORKSPACE_DIR/kApp"
+
+    DEVELOPMENT_REPO_URL="https://$GITHUB_DEVELOPMENT_PAT@github.com/kalisio/development.git"
+else
+    shift $((OPTIND-1))
+    WORKSPACE_DIR="$1"
+
+    DEVELOPMENT_REPO_URL="$GITHUB_URL/kalisio/development.git"
+
+    # unset KALISIO_DEVELOPMENT_DIR because we want kli to clone everyhting in $WORKSPACE_DIR
+    unset KALISIO_DEVELOPMENT_DIR
+fi
+
+setup_app_workspace \
+    "$ROOT_DIR" \
+    "$WORKSPACE_DIR" \
+    "$DEVELOPMENT_REPO_URL" \
+    "$WORKSPACE_NODE" \
+    "workspaces/apps" \
+    "$WORKSPACE_KIND" \
+    "${WORKSPACE_TAG:-$WORKSPACE_BRANCH}"
+
+end_group "Setting up workspace ..."
