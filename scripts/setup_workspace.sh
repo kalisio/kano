5c51d3c2ad26d5d5a0e69b7ffab061c895dbd927
Robin Bourianes <robin.bourianes@kalisio.com>
2024-06-07 12:08:45 +0200

    ci: now using KALISIO_GITHUB_URL

--- scripts/setup_workspace.sh
+++ scripts/setup_workspace.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
 THIS_DIR=$(dirname "$THIS_FILE")
 ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
 
 . "$THIS_DIR/kash/kash.sh"
 
@@ -38,9 +39,6 @@ done
 begin_group "Setting up workspace ..."
 
 if [ "$CI" = true ]; then
-    WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
-    DEVELOPMENT_REPO_URL="https://$GITHUB_DEVELOPMENT_TOKEN@github.com/kalisio/development.git"
-
     # workaround since repo is named 'kano' and in kli file it's 'maps'
     cd "$WORKSPACE_DIR"
     mv "kano" "maps" && ln -s "maps" "kano"
@@ -48,7 +46,6 @@ if [ "$CI" = true ]; then
 else
     shift $((OPTIND-1))
     WORKSPACE_DIR="$1"
-    DEVELOPMENT_REPO_URL="$GITHUB_URL/kalisio/development.git"
 
     # unset KALISIO_DEVELOPMENT_DIR because we want kli to clone everyhting in $WORKSPACE_DIR
     unset KALISIO_DEVELOPMENT_DIR
@@ -57,7 +54,7 @@ fi
 setup_app_workspace \
     "$ROOT_DIR" \
     "$WORKSPACE_DIR" \
-    "$DEVELOPMENT_REPO_URL" \
+    "$KALISIO_GITHUB_URL/kalisio/development.git" \
     "$WORKSPACE_NODE" \
     "workspaces/apps" \
     "$WORKSPACE_KIND" \
