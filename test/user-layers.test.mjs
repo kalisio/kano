0356f72a588d125a9d01bdc571254b1e4e1e9597
Luc Claustres <luc.claustres@orange.fr>
2024-08-16 11:51:30 +0200

    chore: improved user layers test with WMTS/WFS layers and legend

--- test/user-layers.test.mjs
+++ test/user-layers.test.mjs
@@ -70,17 +70,7 @@ describe(`suite:${suite}`, () => {
     await map.clickLayer(page, userLayersTab, 'espaces-naturels')
     expect(match).beTrue()
   })
-
-  it('user: connect wms layer', async () => {
-    const service = 'https://geoservices.brgm.fr/geologie?service=wms&request=getcapabilities'
-    const layerId = 'geologie'
-    const layerName = 'Cartes géologiques'
-    await map.connectLayer(page, service, layerId, layerName)
-    const match = await runner.captureAndMatch(layerId)
-    await map.clickLayer(page, userLayersTab, _.kebabCase('Cartes géologiques'))
-    expect(match).beTrue()
-  })
-
+  
   it('user: add polygon mask', async () => {
     await map.dropFile(page, runner.getDataPath('ariege.geojson'))
     const match = await runner.captureAndMatch('polygon-mask')
@@ -93,6 +83,44 @@ describe(`suite:${suite}`, () => {
     const match = await runner.captureAndMatch('multi-polygon-mask')
     expect(match).beTrue()
     await map.removeLayer(page, userLayersTab, 'occitanie')
+  })
+
+  it('user: connect wms layer', async () => {
+    await core.clickPaneActions(page, 'top', ['tools', 'display-legend'])
+    await map.goToPosition(page, 46.62681, 2.59277)
+    await map.zoomToLevel(page, 7)
+    const service = 'https://geoservices.brgm.fr/geologie?service=wms&request=getcapabilities'
+    const layerId = 'litho-1-m-simplifiee'
+    const layerName = 'Carte lithologique'
+    await map.connectLayer(page, service, layerId, layerName)
+    const match = await runner.captureAndMatch(layerId)
+    await core.closeWindow(page, 'left')
+    await map.clickLayer(page, userLayersTab, _.kebabCase('Carte lithologique simplifiée au 1/1 000 000'))
+    expect(match).beTrue()
+  })
+  it('user: connect wfs layer', async () => {
+    await core.clickPaneActions(page, 'top', ['tools', 'display-legend'])
+    const service = 'https://wxs.ign.fr/choisirgeoportail/geoportail/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetCapabilities'
+    const layerId = 'bdcarto-v-5-region'
+    const layerName = 'region'
+    await map.connectLayer(page, service, layerId, layerName, 'code-insee')
+    const match = await runner.captureAndMatch(layerId)
+    await core.closeWindow(page, 'left')
+    await map.clickLayer(page, userLayersTab, _.kebabCase('region'))
+    expect(match).beTrue()
+  })
+  it('user: connect wmts layer', async () => {
+    await core.clickPaneActions(page, 'top', ['tools', 'display-legend'])
+    await map.goToPosition(page, 46.83201, 8.31116)
+    await map.zoomToLevel(page, 8)
+    const service = 'https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml'
+    const layerId = 'ch-bakom-notruf-112-zentral'
+    const layerName = '112 Alarmzentralen'
+    await map.connectLayer(page, service, layerId, layerName)
+    const match = await runner.captureAndMatch(layerId)
+    await core.closeWindow(page, 'left')
+    await map.clickLayer(page, userLayersTab, _.kebabCase('112 Alarmzentralen'))
+    expect(match).beTrue()
   })
 
   after(async () => {
