188c1563a123efa8d84d936b1a337ba32c366c3e
Luc Claustres <luc.claustres@orange.fr>
2024-10-08 19:05:24 +0200

    wip: Allow to make layers available offline - simplification refactoring and fixed various issues (#353) [skip ci]

--- src/services/index.js
+++ src/services/index.js
@@ -1,42 +1,6 @@
 import logger from 'loglevel'
-import kdkCore, { hooks as kCoreHooks } from '@kalisio/kdk/core.client'
-import kdkMap, { hooks as kMapHooks } from '@kalisio/kdk/map.client.map'
-import { LocalForage } from '@kalisio/feathers-localforage'
-
-async function createOfflineServices (api) {
-  const services = await LocalForage.getItem('services')
-  if (services) {
-    const serviceNames = Object.keys(services)
-    for (let i = 0; i < serviceNames.length; i++) {
-      const serviceName = serviceNames[i]
-      const service = services[serviceName]
-      if (service.layerService) {
-        await api.createOfflineService(serviceName, {
-          snapshot: false,
-          hooks: {
-            before: {
-              all: [kCoreHooks.removeServerSideParameters, kMapHooks.removeServerSideParameters],
-              create: kMapHooks.referenceCountCreateHook,
-              remove: kMapHooks.referenceCountRemoveHook
-            },
-            after: {
-              find: [kMapHooks.geoJsonPaginationHook, kMapHooks.intersectBBoxHook]
-            }
-          }
-        })
-      } else {
-        await api.createOfflineService(serviceName, {
-          snapshot: false,
-          hooks: {
-            before: {
-              all: [kCoreHooks.removeServerSideParameters, kMapHooks.removeServerSideParameters]
-            }
-          }
-        })
-      }
-    }
-  }
-}
+import kdkCore from '@kalisio/kdk/core.client'
+import kdkMap, { utils as kMapUtils } from '@kalisio/kdk/map.client.map'
 
 export default async function () {
   const api = this
@@ -53,7 +17,7 @@ export default async function () {
     const settingsService = api.getService('settings')
     if (settingsService) settingsService.restoreSettings()
     // Create required services for offline mode
-    createOfflineServices(api)
+    kMapUtils.createOfflineServices()
   } catch (error) {
     logger.error(error.message)
   }
