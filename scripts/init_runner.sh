e48930c66402df50738ac780dcdabe1533d26b6f
Robin Bourianes <robin.bourianes@kalisio.com>
2024-10-17 12:06:18 +0200

    ci: add backend tests run

--- scripts/init_runner.sh
+++ scripts/init_runner.sh
@@ -11,6 +11,10 @@ THIS_DIR=$(dirname "$THIS_FILE")
 
 ### Github Actions
 
+init_github_run_tests() {
+    install_reqs age sops nvm node20 mongo7 cc_test_reporter
+}
+
 init_github_build_app() {
     install_reqs age sops nvm node20
 }
---
+++ scripts/run_tests.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
+. "$THIS_DIR/kash/kash.sh"
+
+## Parse options
+##
+
+NODE_VER=20
+MONGO_VER=7
+CI_STEP_NAME="Run tests"
+CODE_COVERAGE=false
+while getopts "m:n:cr:" option; do
+    case $option in
+        m) # defines mongo version
+            MONGO_VER=$OPTARG
+            ;;
+        n) # defines node version
+            NODE_VER=$OPTARG
+             ;;
+        c) # publish code coverage
+            CODE_COVERAGE=true
+            ;;
+        r) # report outcome to slack
+            CI_STEP_NAME=$OPTARG
+            load_env_files "$WORKSPACE_DIR/development/common/SLACK_WEBHOOK_APPS.enc.env"
+            trap 'slack_ci_report "$ROOT_DIR" "$CI_STEP_NAME" "$?" "$SLACK_WEBHOOK_APPS"' EXIT
+            ;;
+        *)
+            ;;
+    esac
+done
+
+
+## Init workspace
+##
+
+. "$WORKSPACE_DIR/development/workspaces/apps/apps.sh" maps
+
+## Run tests
+##
+
+run_app_tests "$ROOT_DIR" "workspaces/apps" "$CODE_COVERAGE" "$NODE_VER" "$MONGO_VER"
