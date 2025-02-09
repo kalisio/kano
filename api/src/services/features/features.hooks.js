abedb08f97440be309da2acb1c2d591c9f6f8613
Luc Claustres <luc.claustres@orange.fr>
2025-02-09 21:24:25 +0100

    refactor: removed upsert hook on features now integrated into KDK

--- api/src/services/features/features.hooks.js
+++ api/src/services/features/features.hooks.js
@@ -1,28 +1,19 @@
 import _ from 'lodash'
 import commonHooks from 'feathers-hooks-common'
 
-// Allow upsert if required
-const upsert = (hook) => {
-  _.set(hook, 'params.mongodb', { upsert: _.get(hook, 'params.query.upsert', false) })
-  _.unset(hook, 'params.query.upsert')
-}
-
 export default {
   before: {
     all: [],
     find: [],
     get: [],
     create: [
-      upsert,
       commonHooks.setNow('createdAt', 'updatedAt')
     ],
     update: [
-      upsert,
       commonHooks.discard('createdAt', 'updatedAt'),
       commonHooks.setNow('updatedAt')
     ],
     patch: [
-      upsert,
       commonHooks.discard('createdAt', 'updatedAt'),
       commonHooks.setNow('updatedAt')
     ],
