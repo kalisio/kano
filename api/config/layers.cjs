1c3dcf45aaa61fbaa4e092f5a25900cfec7b9096
Luc Claustres <luc.claustres@orange.fr>
2025-11-07 12:12:23 +0100

    feat: make K2/S3 URL configurable for local development

--- api/config/layers.cjs
+++ api/config/layers.cjs
@@ -8,7 +8,18 @@ const wmtsUrl = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/w
 const tmsUrl = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/tms/1.0.0' : 'https://mapcache.' + kargoDomain + '/mapcache/tms/1.0.0')
 const wmsUrl = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/wms' : 'https://mapcache.' + kargoDomain + '/mapcache')
 const wcsUrl = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/wcs' : 'https://mapserver.' + kargoDomain + '/cgi-bin/ows')
-const k2Url = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/k2' : 'https://k2.' + kargoDomain)
-const s3Url = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/s3' : 'https://s3.eu-central-1.amazonaws.com')
+let k2Url = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/k2' : 'https://k2.' + kargoDomain)
+let s3Url = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/s3' : 'https://s3.eu-central-1.amazonaws.com')
+
+// Allow to locally override K2
+if (process.env.K2_URL) {
+	k2Url = process.env.K2_URL
+	console.log(`Using custom K2 URL ${k2Url}`)
+}
+// Allow to locally override S3
+if (process.env.S3_URL) {
+	s3Url = process.env.S3_URL
+	console.log(`Using custom S3 URL ${s3Url}`)
+}
 
 module.exports = getLayers(glob.sync(path.join(__dirname, 'layers/**/*.cjs')), { wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url })
