d6fa177a2aa4398c140f357384377d8d5e7bd945
Luc Claustres <luc.claustres@orange.fr>
2025-12-15 16:13:00 +0100

    fix: correctly update test DB name when not default one

--- api/test/config/default.cjs
+++ api/test/config/default.cjs
@@ -5,8 +5,8 @@ const config = require(path.join(__dirname, '../../config/default.cjs'))
 
 // Simply changes outputs so we don't pollute DB, logs, etc.
 config.logs.DailyRotateFile.dirname = path.join(__dirname, '..', 'logs')
-config.db.url = config.db.url.replace('kano', 'kano-test')
-if (config.db.secondaries) config.db.secondaries.data = config.db.secondaries.data.replace('odk', 'odk-test')
+config.db.url = config.db.url + '-test'
+if (config.db.secondaries) config.db.secondaries.data = config.db.secondaries.data + '-test'
 delete config.authentication.defaultUsers
 
 module.exports = config
