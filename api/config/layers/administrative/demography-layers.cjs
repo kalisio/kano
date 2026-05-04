4f263cff8017b25e8d28d762c55e1e283339a4fc
Hugo <hugo.sahuc@kalisio.com>
2026-05-04 15:59:17 +0200

    fix: opacity population

--- api/config/layers/administrative/demography-layers.cjs
+++ api/config/layers/administrative/demography-layers.cjs
@@ -172,6 +172,7 @@ module.exports = function ({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }) {
     leaflet: {
       type: 'tileLayer',
       source: `${tmsUrl}/population-density-europe@GLOBAL_WEBMERCATOR/{z}/{x}/{y}.png`,
+      opacity: 0.7,
       minZoom: 4,
       maxZoom: 18,
       minNativeZoom: 9,
